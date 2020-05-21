d8ee3cea4567a681d20f87a9e55d4ad5579f5954
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-21 12:00:04 +0100

    new controller added

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Emil Vaklinov on 21/05/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+    }
+    
+    @objc func addButtonClicked() {
+            performSegue(withIdentifier: "show", sender: nil)
+        }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+}
